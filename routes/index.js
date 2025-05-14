09dd81718a06a09fa6b5173216f21a767364c0be
Gabriel Feippe <gabriel@feippe.com>
2025-05-14 19:35:37 -0300

    W01 Project: Contacts Part 1

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => { res.send('Hello world'); });
-router.get('/algo', (req, res) => { res.send('Hello algo'); });
+router.use('/contacts', require('./contacts'));
 
 module.exports = router;
